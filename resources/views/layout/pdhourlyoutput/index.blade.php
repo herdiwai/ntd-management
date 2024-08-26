4f9359c3888e2b60a601b97e46f34bb86171ea8a
Muh Arsyal Afifan Ersaputra <ersaputra21@gmail.com>
2024-08-26 07:50:39 +0700

    first commit

--- resources/views/layout/pdhourlyoutput/index.blade.php
+++ resources/views/layout/pdhourlyoutput/index.blade.php
@@ -354,10 +354,10 @@
                     <h4 class="page-title">Production Hourly Output</h4>
                 </div>
                 <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
-                    <a href="https://themeforest.net/item/elite-admin-responsive-dashboard-web-app-kit-/16750820" target="_blank" class="btn btn-danger pull-right m-l-20 btn-rounded btn-outline hidden-xs hidden-sm waves-effect waves-light">Buy Now</a>
+                    {{-- <a href="https://themeforest.net/item/elite-admin-responsive-dashboard-web-app-kit-/16750820" target="_blank" class="btn btn-danger pull-right m-l-20 btn-rounded btn-outline hidden-xs hidden-sm waves-effect waves-light">Buy Now</a> --}}
                     <ol class="breadcrumb">
-                        <li><a href="index.html">Dashboard</a></li>
-                        <li class="active">Dashboard 1</li>
+                        <li><a href="index.html">@yield('title')</a></li>
+                        <li class="active">@yield('title')</li>
                     </ol>
                 </div>
                 <!-- /.col-lg-12 -->
